71ad58e4a259811780968446d72af157b1ab7c53
deks23 <deks232@gmail.com>
2018-01-19 01:41:30 +0100

    Added own exception

--- Fifth.java
+++ Fifth.java
@@ -1,21 +1,58 @@
 import java.util.*;
-import java.io.*;
-import java.lang.*;
+
 
 public class Fifth {
 	static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Vector<Double> x = new Vector<Double>();
+		Vector<Double> xVector = new Vector<Double>();
+		Vector<Double> yVector = new Vector<Double>();
+	
+		xVector=stringArrayToVector();
+		System.out.println("Vector X: ");
+		System.out.println(xVector);
 		
-		String line = null;
-		line=input.nextLine();
-		String[] stringArray = line.split(" ");
-		
-		for(int i=0; i<stringArray.length;i++) {
-			x.add(Double.parseDouble(stringArray[i]));
+		yVector=stringArrayToVector();
+		System.out.println("Vector Y: ");
+		System.out.println(yVector);
+		try {
+			compareStringLength(yVector, xVector);
+		} catch (WektoryRoznejDlugosciException e) {
+			System.out.println(e.getMessage());
 		}
 		
 	}
 	
+	private static String[] stringToStringArray(String inputString) {
+		String[] stringArray = inputString.split(" ");
+		return stringArray;
+	}
 	
+	private static Vector<Double> stringArrayToVector(){
+		Vector<Double> vector = new Vector<Double>();
+		boolean isCorrect=false;
+		while(!isCorrect) {
+			String line= input.nextLine();
+			String[] stringArray=stringToStringArray(line);
+			
+			try {
+			for(int i=0; i<stringArray.length;i++) {
+				vector.add(Double.parseDouble(stringArray[i]));
+			}
+			isCorrect=true;
+			}catch(NumberFormatException e) {
+				System.out.println("Niepoprawny ciag");
+				vector.removeAllElements();
+				isCorrect=false;
+			}
+		}
+		return vector;
+	}
+	
+	
+	private static boolean compareStringLength(Vector<Double> x, Vector<Double> y) throws WektoryRoznejDlugosciException {
+		if (x.size()==y.size()) return true;
+		else throw new WektoryRoznejDlugosciException();
+	}
+
 }
---
+++ WektoryRoznejDlugosciException.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+
+public class WektoryRoznejDlugosciException extends Exception {
+
+public WektoryRoznejDlugosciException () {
+	super("Wektory nie sa rownej dlugosci, podaj ponownie wektory");
+}
+
+}
